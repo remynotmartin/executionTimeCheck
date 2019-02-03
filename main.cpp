ef06b8ac8becb3b626b50ab2cd4bc5cf39350dbe
Remy Dinh <remy_dinh@yahoo.com>
2019-02-03 04:59:39 -0800

    Reverted back to microseconds

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include <fstream>
 #include <chrono>
 #include <cstdio> // To remove the test files with std::remove()
+
 using namespace std::chrono;
 using std::cout;
 using std::endl;
 using std::cerr;
 using std::ofstream;
+using std::setprecision;
 
 bool printNew();
 bool printEnd();
@@ -23,7 +25,8 @@ int main () {
         return -1;
     }
 	// Test for printNew() function
-	auto start1 = high_resolution_clock::now();
+    // Holy smokes, the type of start1 is actually pretty hairy.
+    auto start1 = high_resolution_clock::now();
 	if(!printNew()) return -1;
 	auto stop1 = high_resolution_clock::now();
 	auto duration1 = duration_cast<microseconds>(stop1 - start1);
@@ -36,17 +39,17 @@ int main () {
 	auto start2 = high_resolution_clock::now();
 	if(!printEnd()) return -1;
 	auto stop2 = high_resolution_clock::now();
-	auto duration2 = std::chrono::duration_cast<microseconds>(stop2 - start2);
+	auto duration2 = duration_cast<microseconds>(stop2 - start2);
 	cout    << "endl took: " << duration2.count()
 		    << " microseconds." << endl;
 	logFile << "endl took: " << duration2.count()
 		    << " microseconds." << endl << endl;
     
-    cout    << "This means \\n was " << std::setprecision(2) << std::fixed
+    cout    << "This means \\n was " << setprecision(2) << std::fixed
             <<  static_cast<double>(duration2.count()) / duration1.count()
             << " times faster than endl.\n\n";
     
-    logFile << "This means \\n was " << std::setprecision(2) << std::fixed
+    logFile << "This means \\n was " << setprecision(2) << std::fixed
             <<  static_cast<double>(duration2.count()) / duration1.count()
             << " times faster than endl.\n\n";
 	
