5630c282151feabd19eefe9cd5b903b65cc0815f
Remy Dinh <remy_dinh@yahoo.com>
2019-02-03 04:22:07 -0800

    Updated .gitignore to ignore the log file, and added code to remove the two test files that get created when the test functions are run.

---
+++ cppBuild.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+g++ -Wall -std=c++11 main.cpp
+
+./a.out
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <chrono>
-
+#include <cstdio> // To remove the test files with std::remove()
 using namespace std::chrono;
 using std::cout;
 using std::endl;
@@ -11,28 +12,49 @@ using std::ofstream;
 bool printNew();
 bool printEnd();
 
+const char* const file1 = "./test1.txt";
+const char* const file2 = "./test2.txt";
+const char* const file3 = "./testLog.txt";
+
 int main () {
+    ofstream logFile(file3, std::ios_base::app); // Appends existing log file
+    if (!logFile) {
+        cerr << "Could not open log file!\n";
+        return -1;
+    }
 	// Test for printNew() function
 	auto start1 = high_resolution_clock::now();
 	if(!printNew()) return -1;
 	auto stop1 = high_resolution_clock::now();
 	auto duration1 = duration_cast<microseconds>(stop1 - start1);
-	cout << "\\n took: "<< duration1.count()
-		 << " milliseconds." << '\n';
+	cout    << "\\n took  : "<< duration1.count()
+		    << " ms." << '\n';
+	logFile << "\\n took:   "<< duration1.count()
+		    << " ms." << '\n';
 	
 	// Test for printNew() function
 	auto start2 = high_resolution_clock::now();
 	if(!printEnd()) return -1;
 	auto stop2 = high_resolution_clock::now();
 	auto duration2 = duration_cast<microseconds>(stop2 - start2);
-	cout << "endl took: " << duration2.count()
-		 << " milliseconds." << endl;
+	cout    << "endl took: " << duration2.count()
+		    << " ms." << endl;
+	logFile << "endl took: " << duration2.count()
+		    << " ms." << endl << endl;
+    
+    cout    << "This means \\n was " << std::setprecision(2) << std::fixed
+            <<  static_cast<double>(duration2.count()) / duration1.count()
+            << " times faster than endl.\n\n";
+    
+    logFile << "This means \\n was " << std::setprecision(2) << std::fixed
+            <<  static_cast<double>(duration2.count()) / duration1.count()
+            << " times faster than endl.\n\n";
 	
 	return 0;
 }
 
 bool printNew() {
-	ofstream oFile("./test1.txt");
+	ofstream oFile(file1);
 	if (!oFile) {
 		cerr << "Could not write file." << '\n';
 		return false;
@@ -40,11 +62,12 @@ bool printNew() {
 	for (int i = 0; i < 1000; i++)
 		oFile << "Hello world!\n";
 	oFile.close();
+    std::remove(file1);
 	return true;
 }
 
 bool printEnd() {
-	ofstream oFile("./test2.txt");
+	ofstream oFile(file2);
 	if (!oFile) {
 		cerr << "Could not write file." << endl;
 		return false;
@@ -52,5 +75,6 @@ bool printEnd() {
 	for (int i = 0; i < 1000; i++)
 		oFile << "Hello world!" << endl;
 	oFile.close();
+    std::remove(file2);
 	return true;
 }
